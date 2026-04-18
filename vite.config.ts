e729344cfce02557d719f4081179572ce876c1c7
jaylevaug <126954231+jaylevaug@users.noreply.github.com>
2026-04-18 23:11:58 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  // Verify this matches your GitHub repo name exactly
   base: "/ramos-jaymes-devport/",
   build: {
     outDir: "dist",
-    rollupOptions: {
-      input: {
-        // Change 'index.html' to './src/index.html' if it's in the src folder
-        main: resolve(__dirname, "index.html"), 
-      },
-    },
   },
 });
