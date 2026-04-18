be736213ef48723f95c32014956bdb184eede83d
jaylevaug <126954231+jaylevaug@users.noreply.github.com>
2026-04-19 01:01:12 +0800

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -23,7 +23,7 @@ export const Route = createFileRoute("/")({
   component: HomePage,
 });
 
-const ROLES = ["Digital Literate Educator", "Future Math Teacher", "Lifelong Learner", "Reflective Practitioner"];
+const ROLES = ["a digital literate and date-driven decision maker", "a design thinker and innovator", "a discerning and compassionate person", "a dynamic lifelong learner and leader", "a deeply-rooted global citizen", "a proud PNUan", "a future-ready teacher"];
 
 function Typewriter() {
   const [index, setIndex] = useState(0);
