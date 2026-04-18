4fcf08ba3d6d3809f1ee2b60bf85ad66c04e4294
jaylevaug <126954231+jaylevaug@users.noreply.github.com>
2026-04-18 22:43:22 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  // This ensures your assets (CSS/JS) load from the correct folder on GitHub
   base: "/ramos-jaymes-devport/",
   build: {
-    // This ensures the output goes exactly where we expect it
     outDir: "dist",
+    rollupOptions: {
+      input: {
+        // Change 'index.html' to './src/index.html' if it's in the src folder
+        main: resolve(__dirname, "index.html"), 
+      },
+    },
   },
 });
