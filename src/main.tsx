6b654f668e0a6c206de30688da1701dc293e8119
jaylevaug <126954231+jaylevaug@users.noreply.github.com>
2026-04-18 23:56:08 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
 import { getRouter } from './router'
-import './index.css'
+import './style.css'
 
 const router = getRouter()
 
