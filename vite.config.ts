2122922d03fd19eaf68601577bdb8f444e69334a
jaylevaug <126954231+jaylevaug@users.noreply.github.com>
2026-04-19 00:32:18 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  base: '/ramos-jaymes-devport/', // This tells Vite exactly where the files live
+  // THIS LINE IS THE MOST IMPORTANT:
+  base: '/ramos-jaymes-devport/', 
   plugins: [react(), tsconfigPaths()],
 })
