de9bc6ce4e4958369e77962cf2c722630ede4f48
jaylevaug <126954231+jaylevaug@users.noreply.github.com>
2026-04-18 19:42:19 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   vite: {
+    // This MUST match your repository name exactly
     base: "/ramos-jaymes-devport/",
   },
 });
